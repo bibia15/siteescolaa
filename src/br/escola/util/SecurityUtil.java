5446fdb2af87b94f4e27c04c97561fb95dce5990
bibia15 <163008110+bibia15@users.noreply.github.com>
2025-11-17 11:35:20 -0300

    Create SecurityUtil.java

---
+++ src/br/escola/util/SecurityUtil.java
@@ -0,0 +1,21 @@
+package br.escola.util;
+
+import java.security.MessageDigest;
+
+public class SecurityUtil {
+    public static String sha256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes("UTF-8"));
+            StringBuilder hex = new StringBuilder();
+            for (byte b : hash) {
+                String h = Integer.toHexString(0xff & b);
+                if (h.length() == 1) hex.append('0');
+                hex.append(h);
+            }
+            return hex.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
